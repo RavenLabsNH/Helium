fc8aa5ce5fb0b47987e30b28c446ca7f2813661e
unknown <jeffreyrnelson@outlook.com>
2020-12-01 22:57:16 -0500

    downlink prototype working!

--- Projects/CubeCell/HTCC-AB02S/DowlinkTest/DowlinkTest.ino
+++ Projects/CubeCell/HTCC-AB02S/DowlinkTest/DowlinkTest.ino
@@ -1,14 +1,26 @@
+/* The example is for CubeCell_GPS,
+ * GPS works only before lorawan uplink, the board current is about 45uA when in lowpower mode.
+ */
 #include "LoRaWan_APP.h"
 #include "Arduino.h"
+#include "GPS_Air530.h"
+#include "cubecell_SSD1306Wire.h"
 
+extern SSD1306Wire  display; 
+
+//when gps waked, if in GPS_UPDATE_TIMEOUT, gps not fixed then into low power mode
+#define GPS_UPDATE_TIMEOUT 5000
+
+//once fixed, GPS_CONTINUE_TIME later into low power mode
+#define GPS_CONTINUE_TIME 700
 /*
- * set LoraWan_RGB to Active,the RGB active in loraWan
- * RGB red means sending;
- * RGB purple means joined done;
- * RGB blue means RxWindow1;
- * RGB yellow means RxWindow2;
- * RGB green means received done;
- */
+   set LoraWan_RGB to Active,the RGB active in loraWan
+   RGB red means sending;
+   RGB purple means joined done;
+   RGB blue means RxWindow1;
+   RGB yellow means RxWindow2;
+   RGB green means received done;
+*/
 
 /* OTAA para*/
 uint8_t devEui[] = { 0x81, 0x11, 0x19, 0x2A, 0x86, 0x08, 0xFC, 0xC0 };
@@ -16,9 +28,9 @@ uint8_t appEui[] = { 0x68, 0x2F, 0x51, 0xC0, 0x67, 0x9C, 0xF2, 0x93 };
 uint8_t appKey[] = { 0x4D, 0xA5, 0x54, 0x5F, 0xB3, 0x09, 0xDB, 0x46, 0xBE, 0x30, 0x23, 0xC7, 0x5D, 0xA5, 0x37, 0x24 };
 
 /* ABP para*/
-uint8_t nwkSKey[] = { 0x15, 0xb1, 0xd0, 0xef, 0xa4, 0x63, 0xdf, 0xbe, 0x3d, 0x11, 0x18, 0x1e, 0x1e, 0xc7, 0xda,0x85 };
-uint8_t appSKey[] = { 0xd7, 0x2c, 0x78, 0x75, 0x8c, 0xdc, 0xca, 0xbf, 0x55, 0xee, 0x4a, 0x77, 0x8d, 0x16, 0xef,0x67 };
-uint32_t devAddr =  ( uint32_t )0x007e6ae1;
+uint8_t nwkSKey[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+uint8_t appSKey[] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+uint32_t devAddr =  ( uint32_t )0x00000000;
 
 /*LoraWan channelsmask, default channels 0-7*/ 
 uint16_t userChannelsMask[6]={ 0xFF00,0x0000,0x0000,0x0000,0x0000,0x0000 };
@@ -30,7 +42,7 @@ LoRaMacRegion_t loraWanRegion = ACTIVE_REGION;
 DeviceClass_t  loraWanClass = LORAWAN_CLASS;
 
 /*the application data transmission duty cycle.  value in [ms].*/
-uint32_t appTxDutyCycle = 15000;
+uint32_t appTxDutyCycle = 8700;
 
 /*OTAA or ABP*/
 bool overTheAirActivation = LORAWAN_NETMODE;
@@ -47,42 +59,265 @@ bool isTxConfirmed = LORAWAN_UPLINKMODE;
 /* Application port */
 uint8_t appPort = 2;
 /*!
-* Number of trials to transmit the frame, if the LoRaMAC layer did not
-* receive an acknowledgment. The MAC performs a datarate adaptation,
-* according to the LoRaWAN Specification V1.0.2, chapter 18.4, according
-* to the following table:
-*
-* Transmission nb | Data Rate
-* ----------------|-----------
-* 1 (first)       | DR
-* 2               | DR
-* 3               | max(DR-1,0)
-* 4               | max(DR-1,0)
-* 5               | max(DR-2,0)
-* 6               | max(DR-2,0)
-* 7               | max(DR-3,0)
-* 8               | max(DR-3,0)
-*
-* Note, that if NbTrials is set to 1 or 2, the MAC will not decrease
-* the datarate, in case the LoRaMAC layer did not receive an acknowledgment
+  Number of trials to transmit the frame, if the LoRaMAC layer did not
+  receive an acknowledgment. The MAC performs a datarate adaptation,
+  according to the LoRaWAN Specification V1.0.2, chapter 18.4, according
+  to the following table:
+
+  Transmission nb | Data Rate
+  ----------------|-----------
+  1 (first)       | DR
+  2               | DR
+  3               | max(DR-1,0)
+  4               | max(DR-1,0)
+  5               | max(DR-2,0)
+  6               | max(DR-2,0)
+  7               | max(DR-3,0)
+  8               | max(DR-3,0)
+
+  Note, that if NbTrials is set to 1 or 2, the MAC will not decrease
+  the datarate, in case the LoRaMAC layer did not receive an acknowledgment
 */
 uint8_t confirmedNbTrials = 4;
 
-/* Prepares the payload of the frame */
+int32_t fracPart(double val, int n)
+{
+  return (int32_t)((val - (int32_t)(val))*pow(10,n));
+}
+
+void VextON(void)
+{
+  pinMode(Vext,OUTPUT);
+  digitalWrite(Vext, LOW);
+}
+
+void VextOFF(void) //Vext default OFF
+{
+  pinMode(Vext,OUTPUT);
+  digitalWrite(Vext, HIGH);
+}
+void displayGPSInof()
+{
+  char str[30];
+  display.clear();
+  display.setFont(ArialMT_Plain_10);
+  int index = sprintf(str,"%02d-%02d-%02d",Air530.date.year(),Air530.date.day(),Air530.date.month());
+  str[index] = 0;
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.drawString(0, 0, str);
+  
+  index = sprintf(str,"%02d:%02d:%02d",Air530.time.hour(),Air530.time.minute(),Air530.time.second(),Air530.time.centisecond());
+  str[index] = 0;
+  display.drawString(60, 0, str);
+
+  if( Air530.location.age() < 1000 )
+  {
+    display.drawString(120, 0, "A");
+  }
+  else
+  {
+    display.drawString(120, 0, "V");
+  }
+  
+  index = sprintf(str,"alt: %d.%d",(int)Air530.altitude.meters(),fracPart(Air530.altitude.meters(),2));
+  str[index] = 0;
+  display.drawString(0, 16, str);
+   
+  index = sprintf(str,"hdop: %d.%d",(int)Air530.hdop.hdop(),fracPart(Air530.hdop.hdop(),2));
+  str[index] = 0;
+  display.drawString(0, 32, str); 
+ 
+  index = sprintf(str,"lat :  %d.%d",(int)Air530.location.lat(),fracPart(Air530.location.lat(),4));
+  str[index] = 0;
+  display.drawString(60, 16, str);   
+  
+  index = sprintf(str,"lon:%d.%d",(int)Air530.location.lng(),fracPart(Air530.location.lng(),4));
+  str[index] = 0;
+  display.drawString(60, 32, str);
+
+  index = sprintf(str,"speed: %d.%d km/h",(int)Air530.speed.kmph(),fracPart(Air530.speed.kmph(),3));
+  str[index] = 0;
+  display.drawString(0, 48, str);
+  display.display();
+}
+
+void printGPSInof()
+{
+  Serial.print("Date/Time: ");
+  if (Air530.date.isValid())
+  {
+    Serial.printf("%d/%02d/%02d",Air530.date.year(),Air530.date.day(),Air530.date.month());
+  }
+  else
+  {
+    Serial.print("INVALID");
+  }
+
+  if (Air530.time.isValid())
+  {
+    Serial.printf(" %02d:%02d:%02d.%02d",Air530.time.hour(),Air530.time.minute(),Air530.time.second(),Air530.time.centisecond());
+  }
+  else
+  {
+    Serial.print(" INVALID");
+  }
+  Serial.println();
+  
+  Serial.print("LAT: ");
+  Serial.print(Air530.location.lat(),6);
+  Serial.print(", LON: ");
+  Serial.print(Air530.location.lng(),6);
+  Serial.print(", ALT: ");
+  Serial.print(Air530.altitude.meters());
+
+  Serial.println(); 
+  
+  Serial.print("SATS: ");
+  Serial.print(Air530.satellites.value());
+  Serial.print(", HDOP: ");
+  Serial.print(Air530.hdop.hdop());
+  Serial.print(", AGE: ");
+  Serial.print(Air530.location.age());
+  Serial.print(", COURSE: ");
+  Serial.print(Air530.course.deg());
+  Serial.print(", SPEED: ");
+  Serial.println(Air530.speed.kmph());
+  Serial.println();
+}
+
 static void prepareTxFrame( uint8_t port )
 {
-	/*appData size is LORAWAN_APP_DATA_MAX_SIZE which is defined in "commissioning.h".
-	*appDataSize max value is LORAWAN_APP_DATA_MAX_SIZE.
-	*if enabled AT, don't modify LORAWAN_APP_DATA_MAX_SIZE, it may cause system hanging or failure.
-	*if disabled AT, LORAWAN_APP_DATA_MAX_SIZE can be modified, the max value is reference to lorawan region and SF.
-	*for example, if use REGION_CN470, 
-	*the max value for different DR can be found in MaxPayloadOfDatarateCN470 refer to DataratesCN470 and BandwidthsCN470 in "RegionCN470.h".
-	*/
-    appDataSize = 4;
-    appData[0] = 0x00;
-    appData[1] = 0x01;
-    appData[2] = 0x02;
-    appData[3] = 0x03;
+  /*appData size is LORAWAN_APP_DATA_MAX_SIZE which is defined in "commissioning.h".
+    appDataSize max value is LORAWAN_APP_DATA_MAX_SIZE.
+    if enabled AT, don't modify LORAWAN_APP_DATA_MAX_SIZE, it may cause system hanging or failure.
+    if disabled AT, LORAWAN_APP_DATA_MAX_SIZE can be modified, the max value is reference to lorawan region and SF.
+    for example, if use REGION_CN470,
+    the max value for different DR can be found in MaxPayloadOfDatarateCN470 refer to DataratesCN470 and BandwidthsCN470 in "RegionCN470.h".
+  */
+
+  uint32_t lat, lon;
+  int  alt, course, speed, hdop, sats;
+  
+  Serial.println("Waiting for GPS FIX ...");
+
+  VextON();// oled power on;
+  delay(10); 
+  display.init();
+  display.clear();
+      
+  display.setTextAlignment(TEXT_ALIGN_CENTER);
+  display.setFont(ArialMT_Plain_16);
+  display.drawString(64, 32-16/2, "GPS Searching...");
+  Serial.println("GPS Searching...");
+  display.display();
+  
+  Air530.begin();
+
+  uint32_t start = millis();
+  while( (millis()-start) < GPS_UPDATE_TIMEOUT )
+  {
+    while (Air530.available() > 0)
+    {
+      Air530.encode(Air530.read());
+    }
+   // gps fixed in a second
+    if( Air530.location.age() < 1000 )
+    {
+      break;
+    }
+  }
+  
+  //if gps fixed,  GPS_CONTINUE_TIME later stop GPS into low power mode, and every 1 second update gps, print and display gps info
+  if(Air530.location.age() < 1000)
+  {
+    start = millis();
+    uint32_t printinfo = 0;
+    while( (millis()-start) < GPS_CONTINUE_TIME )
+    {
+      while (Air530.available() > 0)
+      {
+        Air530.encode(Air530.read());
+      }
+
+      if( (millis()-start) > printinfo )
+      {
+        printinfo += 1000;
+        printGPSInof();
+        displayGPSInof();
+      }
+    }
+  }
+  else
+  {
+    display.clear();  
+    display.setTextAlignment(TEXT_ALIGN_CENTER);
+    display.setFont(ArialMT_Plain_16);
+    display.drawString(64, 32-16/2, "No GPS signal");
+    Serial.println("No GPS signal");
+    display.display();
+    delay(2000);
+  }
+  Air530.end(); 
+  display.clear();
+  display.display();
+  display.stop();
+  VextOFF(); //oled power off
+  
+  lat = (uint32_t)(Air530.location.lat()*1E7);
+  lon = (uint32_t)(Air530.location.lng()*1E7);
+  alt = (uint16_t)Air530.altitude.meters();
+  course = Air530.course.deg();
+  speed = Air530.speed.kmph();
+  sats = Air530.satellites.value();
+  hdop = Air530.hdop.hdop();
+
+  uint16_t batteryVoltage = getBatteryVoltage();
+  unsigned char *puc;
+  appDataSize = 0;
+
+  puc = (unsigned char *)(&lat);
+  appData[appDataSize++] = puc[3];
+  appData[appDataSize++] = puc[2];
+  appData[appDataSize++] = puc[1];
+  appData[appDataSize++] = puc[0];
+
+  puc = (unsigned char *)(&lon);
+  appData[appDataSize++] = puc[3];
+  appData[appDataSize++] = puc[2];
+  appData[appDataSize++] = puc[1];
+  appData[appDataSize++] = puc[0];
+  
+  puc = (unsigned char *)(&alt);
+  appData[appDataSize++] = puc[1];
+  appData[appDataSize++] = puc[0];
+  
+  puc = (unsigned char *)(&sats);
+  appData[appDataSize++] = puc[1];
+  appData[appDataSize++] = puc[0];
+
+  /*
+  appData[appDataSize++] = (uint8_t)(batteryVoltage >> 8);
+  appData[appDataSize++] = (uint8_t)batteryVoltage;
+  */
+
+  puc = (unsigned char *)(&lat);
+  Serial.println(lat,DEC);
+  
+  Serial.print("Hex");
+  Serial.println(*puc,HEX);
+  Serial.print("Dec");
+  Serial.println(*puc,DEC);
+
+  int temp = ((puc[3] << 24) | (puc[2] << 16) | (puc[1] << 8) | puc[0]);
+  unsigned char *puc2;
+  puc2= (unsigned char *)&temp;
+  
+  // Serial.println(temp);
+//   Serial.println(temp);
+//   Serial.println(&temp); 
+  
+  Serial.print(" BatteryVoltage:");
+  Serial.println(batteryVoltage);
 }
 
 //downlink data handle function example
@@ -92,10 +327,24 @@ void downLinkDataHandle(McpsIndication_t *mcpsIndication)
   Serial.print("+REV DATA:");
   for(uint8_t i=0;i<mcpsIndication->BufferSize;i++)
   {
-    Serial.printf("%02X",mcpsIndication->Buffer[i]);
+    Serial.printf("%c",mcpsIndication->Buffer[i]);
   }
   Serial.println();
   uint32_t color=mcpsIndication->Buffer[0]<<16|mcpsIndication->Buffer[1]<<8|mcpsIndication->Buffer[2];
+  
+  char str[40];
+  char str1[40];
+  strcat(str, " MSG: ");
+  memcpy(str1, mcpsIndication->Buffer, mcpsIndication->BufferSize);
+  strcat(str, str1);
+  display.clear();
+  display.setFont(ArialMT_Plain_10);
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.drawString(18, 40, str);
+  
+  display.display();
+
+  
 #if(LoraWan_RGB==1)
   turnOnRGB(color,5000);
   turnOffRGB();
@@ -103,58 +352,63 @@ void downLinkDataHandle(McpsIndication_t *mcpsIndication)
 }
 
 void setup() {
-	boardInitMcu();
-	Serial.begin(115200);
+  boardInitMcu();
+  Serial.begin(115200);
+
 #if(AT_SUPPORT)
-	enableAt();
+  enableAt();
 #endif
-	deviceState = DEVICE_STATE_INIT;
-	LoRaWAN.ifskipjoin();
+  LoRaWAN.displayMcuInit();
+  deviceState = DEVICE_STATE_INIT;
+  LoRaWAN.ifskipjoin();
 }
 
 void loop()
 {
-	switch( deviceState )
-	{
-		case DEVICE_STATE_INIT:
-		{
+  switch( deviceState )
+  {
+    case DEVICE_STATE_INIT:
+    {
 #if(AT_SUPPORT)
-			getDevParam();
+      getDevParam();
 #endif
-			printDevParam();
-			LoRaWAN.init(loraWanClass,loraWanRegion);
-			deviceState = DEVICE_STATE_JOIN;
-			break;
-		}
-		case DEVICE_STATE_JOIN:
-		{
-			LoRaWAN.join();
-			break;
-		}
-		case DEVICE_STATE_SEND:
-		{
-			prepareTxFrame( appPort );
-			LoRaWAN.send();
-			deviceState = DEVICE_STATE_CYCLE;
-			break;
-		}
-		case DEVICE_STATE_CYCLE:
-		{
-			// Schedule next packet transmission
-			txDutyCycleTime = appTxDutyCycle + randr( 0, APP_TX_DUTYCYCLE_RND );
-			LoRaWAN.cycle(txDutyCycleTime);
-			deviceState = DEVICE_STATE_SLEEP;
-			break;
-		}
-		case DEVICE_STATE_SLEEP:
-		{
-			LoRaWAN.sleep();
-			break;
-		}
-		default:
-		{
-			deviceState = DEVICE_STATE_INIT;
-			break;
-		}
-	}
+      printDevParam();
+      LoRaWAN.init(loraWanClass,loraWanRegion);
+      deviceState = DEVICE_STATE_JOIN;
+      break;
+    }
+    case DEVICE_STATE_JOIN:
+    {
+      LoRaWAN.displayJoining();
+      LoRaWAN.join();
+      break;
+    }
+    case DEVICE_STATE_SEND:
+    {
+      prepareTxFrame( appPort );
+      LoRaWAN.displaySending();
+      LoRaWAN.send();
+      deviceState = DEVICE_STATE_CYCLE;
+      break;
+    }
+    case DEVICE_STATE_CYCLE:
+    {
+      // Schedule next packet transmission
+      txDutyCycleTime = appTxDutyCycle + randr( 0, APP_TX_DUTYCYCLE_RND );
+      LoRaWAN.cycle(txDutyCycleTime);
+      deviceState = DEVICE_STATE_SLEEP;
+      break;
+    }
+    case DEVICE_STATE_SLEEP:
+    {
+      //LoRaWAN.displayAck();
+      LoRaWAN.sleep();
+      break;
+    }
+    default:
+    {
+      deviceState = DEVICE_STATE_INIT;
+      break;
+    }
+  }
 }
